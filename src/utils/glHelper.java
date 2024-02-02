020d9f3b5e19b8f6da9b726469e2e8b64a3846a1
Lo1qSk1dder <105985567+False-kbmb@users.noreply.github.com>
2024-02-02 14:39:06 +0800

    push

---
+++ src/utils/glHelper.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class glHelper {
+    public static long GLFW_MONITOR_NULL = 0;
+    public static long GLFW_SHARE_NULL = 0;
+    public static long GLFW_WINDOW_NULL = 0;
+}
